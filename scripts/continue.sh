e329dfe99a0f2b612fa3b05339822d835476bcfc
Liam Martens <liam@Liams-MacBook-Pro.local>
2017-04-29 13:00:59 +0200

    ZFS fs perl in place replace fix

--- scripts/continue.sh
+++ scripts/continue.sh
@@ -16,6 +16,23 @@ export PHP_TCP=`awk 'END{print $1}' /etc/hosts`
 cat /usr/share/zoneinfo/$TIMEZONE > /etc/localtime
 echo $TIMEZONE > /etc/timezone
 
+# function to check php.ini config and bak
+checkPhpIni() {
+	if [[ `cat /etc/php7/php.ini` =~ "^\s*$" ]]; then
+		mv /etc/php7/php.ini.bak /etc/php7/php.ini
+	else
+		rm /etc/php7/php.ini.bak
+	fi
+}
+# function to check fpm www pool and bak
+checkPhpPool() {
+	if [[ `cat /etc/php7/php-fpm.d/www.conf` =~ "^\s*$" ]]; then
+		mv /etc/php7/php-fpm.d/www.conf.bak /etc/php7/php-fpm.d/www.conf
+	else
+		rm /etc/php7/php-fpm.d/www.conf.bak
+	fi
+}
+
 #
 # set php variables
 #
@@ -31,7 +48,8 @@ for VAR in "${ENV_VARS[@]}"; do
 		PHP_SETTING=$(echo $VAR_NAME | cut -d'_' -f 2-)
 		PHP_SETTING=$(echo $PHP_SETTING | awk '{print tolower($0)}')
 		PHP_SETTING=$(echo $PHP_SETTING | perl -pe "s/__/./")
-		perl -pi -e "s/^$PHP_SETTING\s*=\s*.*/$PHP_SETTING = $VAR_VALUE/gi" /etc/php7/php.ini
+		perl -p -i.bak -e "s/^$PHP_SETTING\s*=\s*.*/$PHP_SETTING = $VAR_VALUE/gi" /etc/php7/php.ini
+		checkPhpIni
 	else
 		WWWVARS="$WWWVARS"$'\n'"env[$VAR_NAME]=$VAR_VALUE"
 	fi
@@ -39,10 +57,12 @@ done
 echo "$WWWVARS" > /etc/php7/php-fpm.d/env.conf
 
 # set php listen port
-perl -pi -e "s/listen\s*=\s*(.+):.+/listen = $PHP_TCP:$PHP_PORT/gi" /etc/php7/php-fpm.d/www.conf
+perl -p -i.bak -e "s/listen\s*=\s*(.+):.+/listen = $PHP_TCP:$PHP_PORT/gi" /etc/php7/php-fpm.d/www.conf
+checkPhpPool
 
 # set timezone
-perl -pi -e "s/;*date.timezone\s*=.*/date.timezone = $TIMEZONE/gi" /etc/php7/php.ini
+perl -p -i.bak -e "s/;*date.timezone\s*=.*/date.timezone = $TIMEZONE/gi" /etc/php7/php.ini
+checkPhpIni
 
 echo "Starting PHP FPM on $PHP_PORT"
 php-fpm7
